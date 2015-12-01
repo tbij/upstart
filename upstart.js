bc99bf61d55fdf468bb94722f87e1a0bab701226
Max Harlow <maxharlow@tbij.com>
2015-12-01 10:57:34 +0000

    Basic functionality

--- upstart.js
+++ upstart.js
@@ -10,6 +10,7 @@ const Multer = require('multer')
 const AdmZip = require('adm-zip')
 const Github = require('github')
 const NodeGit = require('nodegit')
+const Ursa = require('ursa')
 const Config = require('./config')
 
 const cookieOptions = { maxAge: 1000 * 60 * 60 * 24 * 7 } // expires in a week
@@ -65,4 +66,168 @@ app.get('/sign-in/authenticate', signinAuth, (request, response) => {
 
 app.use('/', Express.static('interface'))
 
+app.post('/new', upload, (request, response) => {
+    const name = request.body.name.replace(/[^A-Za-z0-9-]/g, '-').replace(/-+/g, '-').toLowerCase().substring(0, 100)
+    const file = request.file
+    if (name === null || name.length < 1 || file === null || !valid(file)) response.status(400).send('the file was not valid')
+    else create(name, file, request.user)
+        .then(() => response.sendStatus(201))
+        .catch(() => response.status(500).send('something went wrong'))
+})
+
 app.listen(Config.port)
+
+function valid(file) {
+    const fileZip = new AdmZip(file.path)
+    const directory = fileZip.getEntries().find(entry => entry.isDirectory).entryName
+    const noIndex = fileZip.getEntry(directory + 'index.html') === null
+    if (noIndex) return false
+    return true
+}
+
+function create(name, file, user) {
+    const path = createUnzipped(file)
+    return createGithubRepository(name).then(remote => {
+        addGithubKey(name).then(key => {
+            gitInit(path)
+                .then(repository => gitAddAll(repository))
+                .then(repository => gitCommit(repository, user))
+                .then(repository => gitBranch(repository))
+                .then(repository => gitRemote(repository, remote))
+                .then(repository => gitPush(repository, key))
+                .then(() => removeGithubKey(key.id))
+                .then(() => FS.remove(file.path))
+                .then(() => FS.remove(Path.resolve(path, '..')))
+        })
+    })
+}
+
+function createUnzipped(file) {
+    FS.mkdir(file.path + '-x')
+    const fileZip = new AdmZip(file.path)
+    fileZip.extractAllTo(file.path + '-x', true)
+    const directory = fileZip.getEntries().find(entry => entry.isDirectory).entryName
+    return Path.resolve(file.path + '-x/' + directory)
+}
+
+
+function createGithubRepository(name) {
+    const github = new Github({ version: '3.0.0' })
+    github.authenticate({
+        type: 'basic',
+        username: Config.git.username,
+        password: Config.git.password
+    })
+    const config = {
+        org: Config.git.organisation,
+        name: name,
+        has_issues: false,
+        has_wiki: false
+    }
+    return new Promise((resolve, reject) => {
+        github.repos.createFromOrg(config, (e, result) => {
+            if (e) reject(e)
+            else resolve(result.ssh_url)
+        })
+    })
+}
+
+function addGithubKey(name) {
+    const key = Ursa.generatePrivateKey()
+    const keyPublic = 'ssh-rsa ' + key.toPublicSsh().toString('base64')
+    const keyPrivate = key.toPrivatePem().toString()
+    const github = new Github({ version: '3.0.0' })
+    github.authenticate({
+        type: 'basic',
+        username: Config.git.username,
+        password: Config.git.password
+    })
+    const config = {
+        title: name,
+        key: keyPublic
+    }
+    return new Promise((resolve, reject) => {
+        github.user.createKey(config, (e, result) => {
+            if (e) reject(e)
+            else resolve({ id: result.id, public: keyPublic, private: keyPrivate })
+        })
+    })
+}
+
+function removeGithubKey(id) {
+    const github = new Github({ version: '3.0.0' })
+    github.authenticate({
+        type: 'basic',
+        username: Config.git.username,
+        password: Config.git.password
+    })
+    const config = {
+        id: id
+    }
+    return new Promise((resolve, reject) => {
+        github.user.deleteKey(config, (e, result) => {
+            if (e) reject(e)
+            else resolve()
+        })
+    })
+}
+
+function gitInit(path) {
+    return NodeGit.Repository.init(path, 0)
+}
+
+function gitAddAll(repository) {
+    return new Promise((resolve, reject) => {
+        repository.openIndex()
+            .then(index => {
+                index.addAll().then(() => resolve(repository))
+            })
+            .catch(reject)
+    })
+}
+
+function gitCommit(repository, user) {
+    const author = NodeGit.Signature.create(user._json.name, user._json.email, Math.round(Date.now() / 1000), 0)
+    return new Promise((resolve, reject) => {
+        repository.openIndex()
+            .then(index => {
+                index.write()
+                index.writeTree().then(oid => {
+                    repository.createCommit('HEAD', author, author, 'Published', oid, []).then(() => resolve(repository))
+                })
+            })
+            .catch(reject)
+    })
+}
+
+function gitBranch(repository) {
+    return new Promise((resolve, reject) => {
+        repository.getCurrentBranch()
+            .then(master => {
+                NodeGit.Branch.move(master, 'gh-pages', 0).then(() => resolve(repository))
+            })
+            .catch(reject)
+    })
+}
+
+function gitRemote(repository, remote) {
+    return new Promise((resolve, reject) => {
+        NodeGit.Remote.create(repository, 'origin', remote)
+        resolve(repository)
+    })
+}
+
+function gitPush(repository, key) {
+    const config = {
+        callbacks: {
+           credentials: (_, user) => NodeGit.Cred.sshKeyMemoryNew(Config.git.username, key.public, key.private, '')
+        }
+    }
+    return new Promise((resolve, reject) => {
+        repository.getRemote('origin')
+            .then(remote => {
+                remote.push(['refs/heads/gh-pages:refs/heads/gh-pages'], config).then(() => resolve(repository))
+            })
+            .catch(reject)
+    })
+}
